53a5864370eb9768579711fe4f2dd2d9a6cd65d6
Lisa Han <ellesis@gmail.com>
2022-02-19 06:12:11 -0800

    using axios for api

--- README.md
+++ README.md
@@ -15,6 +15,10 @@ npm run dev
 
 ```
 
+# test API
+
+https://jsonplaceholder.typicode.com/
+
 # origin
 
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import axios from 'axios'
 
 export default function Home({posts}) {
   console.log(`>>>: getServerSideProps -> posts`, posts)
@@ -22,11 +23,16 @@ export default function Home({posts}) {
 // }
 
 export const getStaticProps = async () => {
-  // server side rendering
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users`)
-  const posts = await res.json()
-  return {
-    props: {posts},
-    revalidate: 20 // After 20 seconds regenerate data option
+  // static rendering
+  try {
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/users`)
+    const posts = await res.data
+
+    return {
+      props: {posts},
+      revalidate: 20 // After 20 seconds regenerate data option
+    }
+  } catch (err) {
+    return {err}
   }
 }
