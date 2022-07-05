b5e385b42d31898a362fcb3f987cf04e3c301b38
Lisa Han <ellesis@gmail.com>
2022-07-05 08:32:33 -0800

    Create about page for router

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import axios from 'axios'
+import Link from 'next/link'
+import Image from 'next/image'
+import {useRouter} from 'next/router'
+
+import HeadInfo from '../components/HeadInfo'
+
+export default function About() {
+  const router = useRouter()
+  console.log(`>>>: About -> router`, router)
+
+  return (
+    <div>
+      <HeadInfo title="My photos" />
+
+      <h1>About</h1>
+    </div>
+  )
+}
