341235e32402dabeac82ea93f834f384fe35ee5c
Lisa Han <ellesis@gmail.com>
2022-08-22 06:23:43 -0800

    Add option for Warning: Prop `className` did not match. when using styled components with semantic-ui-react

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  },
   images: {
     domains: ['via.placeholder.com']
   }
